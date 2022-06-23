17101b8978b750b313cc09169c7a028b8c03fabb
arifulmalik <mdarifulislammal@u.boisestate.edu>
2022-06-23 15:49:02 -0600

    13. Roman to Integer.py added

---
+++ Easy/13. Roman to Integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        total=0
+        prev=0
+        for r in s:
+            d=roman[r]
+            total+=d
+            if(prev<d):
+                total+=-prev*2
+            prev=d
+        
+        
+        return total    
+        
